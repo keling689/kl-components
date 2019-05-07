90e7ad5f3332981b1f89019f8a9b57de8e82c26e
keling689 <449780270@qq.com>
2019-05-07 10:04:30 +0800

    创建分支，添加文件

---
+++ src/components/kl-table/src/main.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="kl-table">
+		
+	</div>
+</template>
+
+<script>
+	const list = require('../list.json');
+	console.log(list)
+	export default{
+		name:'KlTable',
+		props:{
+			description: {
+				type: String,
+				default: ''
+			  },			
+		},
+		components:{
+			
+		},
+		data(){
+			return {
+				
+			}
+		},
+		computed:{
+			
+		},
+		created(){
+			this.$axios.get('static/data/list.json').then(res=>{
+				console.log(res)
+			}).catch(err=>{
+				console.log(err)
+			})
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import KlTable from '@/components/kl-table/src/main.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+		  {
+		  path: '/klTable',
+		  name: 'klTable',	
+			component:KlTable
+		  // component: () => import('@components/kl-table/src/main.vue')
+		}
   ]
 })
